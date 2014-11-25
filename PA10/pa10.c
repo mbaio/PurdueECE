ae58649d42f64d27594a24a2be6b9cd3f767830e
mbaio <mbaio@purdue.edu>
2014-11-24 22:20:29 -0500

    Added more things to fix stuff

--- PA10/pa10.c
+++ PA10/pa10.c
@@ -9,6 +9,8 @@ int main()
   
   struct YelpDataBST * bst = create_business_bst("shortfilebus.tsv", "shortfilerev.tsv");
   
+  
+  
   char * name = malloc(sizeof(char) * 13);
   strcpy(name,"RUBy's Diner");
   char * zip = malloc(sizeof(char) * 6);
@@ -16,17 +18,15 @@ int main()
   char * state = malloc(sizeof(char) * 3);
   strcpy(state,"AZ");
   
-  struct Business * bus_node = get_business_reviews(bst, name, state, NULL);
-  
- 
+  struct Business * bus_node = get_business_reviews(bst, name, state, zip);
   int ind;
   int rev;
   if (bus_node != NULL && bst != NULL)
   { 
-    printf("\n\nName: %s\n",bus_node->name);
+    printf("\n\nName: %s\nNum locations: %d",bus_node->name,bus_node -> num_locations);
   for (ind = 0; ind < bus_node->num_locations;ind++)
   {
-    printf("\n\nLocation[%d]: \nAddress: %s\nCity: %s\nState: %s\nZip: %s\n",ind,bus_node->locations[ind].address,bus_node->locations[ind].city,bus_node->locations[ind].state,bus_node->locations[ind].zip_code);   
+    printf("\n\nLocation[%d]: \nAddress: %s\nCity: %s\nState: %s\nZip: %s\nNum reviews = %d\n",ind,bus_node->locations[ind].address,bus_node->locations[ind].city,bus_node->locations[ind].state,bus_node->locations[ind].zip_code,bus_node->locations[ind].num_reviews);   
     for (rev = 0; rev < bus_node->locations[ind].num_reviews;rev++)
     {
       printf("\nReview %d: %s\n\n",rev,bus_node->locations[ind].reviews[rev]); 
@@ -34,6 +34,9 @@ int main()
   }
   printf("\n\n");
   }
+  
+  
+  
   
   
   destroy_business_bst(bst);
